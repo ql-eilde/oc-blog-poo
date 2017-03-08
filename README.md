6bfe99754ad1919739a572167f49e2d704d2bc25
ql-eilde <quentin.leilde@gmail.com>
2017-03-08 16:45:09 +0100

    update readme

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ This is the public repository of my 5th project on OpenClassrooms. The goal of t
 # Installation guide
 1- Git clone the repository into your WAMP or MAMP directory.
 
-2- Open MySQL and do the followings :
-  - Create a database called "blog".
-  - Create the "posts" table by executing this request :
+2- Create a database.
+
+3- Update the file lib/DBFactory.php with your database credentials.
+
+4- Create the "posts" table by executing this request :
   
   CREATE TABLE `posts` (
   `id` smallint(5) unsigned NOT NULL,
@@ -16,6 +18,16 @@ This is the public repository of my 5th project on OpenClassrooms. The goal of t
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL
   ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+  - Then populate the table by adding some fake data with this request :
+
+  INSERT INTO `posts` (`id`, `author`, `title`, `subtitle`, `content`, `createdAt`, `updatedAt`) VALUES
+  (1, 'Quentin L''eilde', 'Lorem Ipsum Titre 1', 'Lorem Ipsum Sous Titre 1', 'Laboriosam consequatur saepe veritatis enim doloribus voluptatem expedita. Deleniti saepe deleniti quae voluptas nihil. Dolor quis quidem consequatur architecto eos dolorem. Quaerat fugit qui exercitationem corporis. Mollitia expedita pariatur eius non temporibus consectetur.', '2017-03-08 16:36:17', '2017-03-08 16:36:17'),
+  (2, 'Quentin L''eilde', 'Lorem Ipsum Titre 2', 'Lorem Ipsum Sous Titre 2', 'Laboriosam consequatur saepe veritatis enim doloribus voluptatem expedita. Deleniti saepe deleniti quae voluptas nihil. Dolor quis quidem consequatur architecto eos dolorem. Quaerat fugit qui exercitationem corporis. Mollitia expedita pariatur eius non temporibus consectetur.', '2017-03-08 16:36:17', '2017-03-08 16:36:17'),
+  (3, 'Quentin L''eilde', 'Lorem Ipsum Titre 3', 'Lorem Ipsum Sous Titre 3', 'Laboriosam consequatur saepe veritatis enim doloribus voluptatem expedita. Deleniti saepe deleniti quae voluptas nihil. Dolor quis quidem consequatur architecto eos dolorem. Quaerat fugit qui exercitationem corporis. Mollitia expedita pariatur eius non temporibus consectetur.', '2017-03-08 16:36:17', '2017-03-08 16:36:17'),
+  (4, 'Quentin L''eilde', 'Lorem Ipsum Titre 4', 'Lorem Ipsum Sous Titre 4', 'Laboriosam consequatur saepe veritatis enim doloribus voluptatem expedita. Deleniti saepe deleniti quae voluptas nihil. Dolor quis quidem consequatur architecto eos dolorem. Quaerat fugit qui exercitationem corporis. Mollitia expedita pariatur eius non temporibus consectetur.', '2017-03-08 16:36:17', '2017-03-08 16:36:17'),
+  (5, 'Quentin L''eilde', 'Lorem Ipsum Titre 5', 'Lorem Ipsum Sous Titre 5', 'Laboriosam consequatur saepe veritatis enim doloribus voluptatem expedita. Deleniti saepe deleniti quae voluptas nihil. Dolor quis quidem consequatur architecto eos dolorem. Quaerat fugit qui exercitationem corporis. Mollitia expedita pariatur eius non temporibus consectetur.', '2017-03-08 16:36:17', '2017-03-08 16:36:17'),
+  (6, 'Quentin L''eilde', 'Lorem Ipsum Titre 6', 'Lorem Ipsum Sous Titre 6', 'Laboriosam consequatur saepe veritatis enim doloribus voluptatem expedita. Deleniti saepe deleniti quae voluptas nihil. Dolor quis quidem consequatur architecto eos dolorem. Quaerat fugit qui exercitationem corporis. Mollitia expedita pariatur eius non temporibus consectetur.', '2017-03-08 16:36:17', '2017-03-08 16:36:17');
   
   - Open the file index.php
   - Bazinga !
--- blog.php
+++ blog.php
@@ -39,49 +39,53 @@ else
     ?>
     <section id="blog-posts">
         <div class="container">
-    <?php
-    $nb_posts = $manager->count();
-    $limit = 5;
-    $nb_pages = ceil($nb_posts / $limit);
-    $page = min($nb_pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array(
-        'options' => array(
-            'default' => 1,
-            'min_range' => 1,
-            ),
-        )));
-    $offset = ($page - 1) * $limit;
-    $start = $offset + 1;
-    $end = min(($offset + $limit), $nb_posts);
-    $prevlink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
-    $nextlink = ($page < $nb_pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a> <a href="?page=' . $nb_pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
-    foreach ($manager->getList($offset, $limit) as $post) {
-        if (strlen($post->content()) <= 200) {
-            $contenu = $post->content();
-        } else {
-            $debut = substr($post->content(), 0, 200);
-            $debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
+            <?php
+            $nb_posts = $manager->count();
+            if($nb_posts > 0){
+                $limit = 5;
+                $nb_pages = ceil($nb_posts / $limit);
+                $page = min($nb_pages, filter_input(INPUT_GET, 'page', FILTER_VALIDATE_INT, array(
+                    'options' => array(
+                        'default' => 1,
+                        'min_range' => 1,
+                        ),
+                    )));
+                $offset = ($page - 1) * $limit;
+                $start = $offset + 1;
+                $end = min(($offset + $limit), $nb_posts);
+                $prevlink = ($page > 1) ? '<a href="?page=1" title="First page">&laquo;</a> <a href="?page=' . ($page - 1) . '" title="Previous page">&lsaquo;</a>' : '<span class="disabled">&laquo;</span> <span class="disabled">&lsaquo;</span>';
+                $nextlink = ($page < $nb_pages) ? '<a href="?page=' . ($page + 1) . '" title="Next page">&rsaquo;</a> <a href="?page=' . $nb_pages . '" title="Last page">&raquo;</a>' : '<span class="disabled">&rsaquo;</span> <span class="disabled">&raquo;</span>';
+                foreach ($manager->getList($offset, $limit) as $post) {
+                    if (strlen($post->content()) <= 200) {
+                        $contenu = $post->content();
+                    } else {
+                        $debut = substr($post->content(), 0, 200);
+                        $debut = substr($debut, 0, strrpos($debut, ' ')) . '...';
 
-            $contenu = $debut;
-        }
-        echo '
-            <div class="row" style="padding-top:20px">
-                <div class="col-lg-12">
-                    <div class="posts" style="margin-bottom:60px">
-                            <h1>' . htmlspecialchars($post->title()) . '</h1>
-                            <p><span class="glyphicon glyphicon-time"></span> Article posté le ' . $post->updatedAt()->format('d/m/Y à H\hi') . '</p>
-                            <hr>
-                            <p class="lead">' . htmlspecialchars($post->subtitle()) . '</p>
-                            <p style="text-align:right"><a class="btn btn-primary" href="?id=' . $post->id() . '">Voir l\'article<span class="glyphicon glyphicon-chevron-right"></span></a></p>
-                            <hr>
-                    </div>
-                </div>
-            </div>';
-    }
-    echo '
-    <div id="paging">
-        <p>', $prevlink, ' Page ', $page, ' sur ', $nb_pages, ' ', $nextlink, '</p>
-    </div>';
-    ?>
+                        $contenu = $debut;
+                    }
+                    echo '
+                        <div class="row" style="padding-top:20px">
+                            <div class="col-lg-12">
+                                <div class="posts" style="margin-bottom:60px">
+                                        <h1>' . htmlspecialchars($post->title()) . '</h1>
+                                        <p><span class="glyphicon glyphicon-time"></span> Article posté le ' . $post->updatedAt()->format('d/m/Y à H\hi') . '</p>
+                                        <hr>
+                                        <p class="lead">' . htmlspecialchars($post->subtitle()) . '</p>
+                                        <p style="text-align:right"><a class="btn btn-primary" href="?id=' . $post->id() . '">Voir l\'article<span class="glyphicon glyphicon-chevron-right"></span></a></p>
+                                        <hr>
+                                </div>
+                            </div>
+                        </div>';
+                }
+                echo '
+                <div id="paging">
+                    <p>', $prevlink, ' Page ', $page, ' sur ', $nb_pages, ' ', $nextlink, '</p>
+                </div>';
+            } else {
+                echo '<h1>Aucun article pour le moment</h1>';
+            }
+            ?>
         </div>
     </section>
 <?php
